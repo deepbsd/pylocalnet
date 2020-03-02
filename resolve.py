ad520230d05e9e476427a0d33c2b92470556e270
David S. Jackson <deepbsd@yahoo.com>
2020-03-02 18:00:06 -0500

    added try/except got script

--- resolve.py
+++ resolve.py
@@ -3,17 +3,19 @@
 import socket
 
 hosts = ['zencat','scee','pumpkin','juno','jarhead',
-        'dixie','peach','peanut','erica','debbie',
-        'blub','omalley','Maui','daisy','milo']
+        'dixie', 'peach', 'peanut','debbie', 'blub',
+        'omalley','Maui','daisy','milo','tweety', 'erica' ]
+
+#hosts = ['zencat','scee']
 
 local_dict = {}
 
-def resolve(hostname):
-    return socket.gethostbyname(hostname)
-
 for host in hosts:
-    ip = socket.gethostbyname(host)
-    local_dict[host] = str(ip)
+    try:
+        ip = socket.gethostbyname(host)
+    except:
+        ip = 'host not available'
+    local_dict[host] = ip
 
 for hostname, ip in local_dict.items():
     print("{0}: {1}".format(hostname, ip))
