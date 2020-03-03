115e45dc2427ab98ac6726f0a14a2669d414d597
David S. Jackson <deepbsd@yahoo.com>
2020-03-02 22:25:42 -0500

    changed .lan so variable is unaltered; also added line numbers and some formatting

--- resolve.py
+++ resolve.py
@@ -5,17 +5,16 @@ import socket
 hosts = ['zencat','scee','pumpkin','juno','jarhead',
         'dixie', 'peach', 'peanut','debbie', 'blub',
         'omalley','Maui','daisy','milo','tweety', 
-        'erica', 'kitty', 'jazzcat' ]
+        'erica', 'kitty', 'jazzcat', 'dolly' ]
 
 local_dict = {}
 
 for host in hosts:
     try:
-        host = str(host+'.lan')
-        ip = socket.gethostbyname(host)
+        ip = socket.gethostbyname(str(host+'.lan'))
     except:
         ip = 'host not available'
     local_dict[host] = ip
 
 for hostname, ip in local_dict.items():
-    print("{0}: {1}".format(hostname, ip))
+    print("{0}:   \t{1}".format(hostname, ip))
